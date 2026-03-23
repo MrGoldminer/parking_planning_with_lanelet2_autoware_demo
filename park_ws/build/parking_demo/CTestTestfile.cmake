240d2fac08d1ca6765d88823610753124fde6a3a
goldminer <18534792564@163.com>
2026-03-23 21:30:03 +0800

    fix: remove emoji,fix parking final approach alignment

--- park_ws/build/parking_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/goldminer/park_ws/src/parking_demo
-# Build directory: /home/goldminer/park_ws/build/parking_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- park_wsv22/park_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/goldminer/park_ws/src
-# Build directory: /home/goldminer/park_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("parking_demo")
